76a61e3cd57709b2ca79df3aa450fb96569a2cc9
Vyacheslav Panchenko <vyacheslav.panchenko@valor-software.com>
2016-12-20 19:02:58 +0200

    chore(language): detect default browser

--- src/app/header/language-switcher/language-switcher.service.ts
+++ src/app/header/language-switcher/language-switcher.service.ts
@@ -22,7 +22,7 @@ export class LanguageSwitcherService {
 
     this.router.events.subscribe((event: NavigationEvent) => {
       if(event instanceof NavigationEnd) {
-        const langFromUrl = this._detectLanguage();
+        const langFromUrl = this.detectLanguage();
         if(
           // language is not defined yet
           !this.Language ||
@@ -52,21 +52,55 @@ export class LanguageSwitcherService {
     return this.Language;
   }
 
-  private _detectLanguage() {
+  private detectLanguage() {
+    let langFound = false;
+
+    // top priority language from url
     const hash = window.location.hash;
     const hashPosition = hash.indexOf("#") + 1;
     const hashModelString = hash.substring(hashPosition);
     const hashModel = this.vService.stringToModel(hashModelString);
 
-    let resultItem = _.first(this.LanguageList);
-
-    if(!!hashModel.locale && !!hashModel.locale.id) {
-      const foundItem = _.find(this.getList(), function (langItem) {
-        return langItem.key === hashModel.locale.id;
+    if (hashModel.locale) {
+      langFound = _.find(this.LanguageList, function (localeItem) {
+        return localeItem.key === hashModel.locale.id;
       });
-      return foundItem ? foundItem : resultItem;
     }
 
-    return resultItem;
+    // second location browser locale
+    const browserLanguage = this.detectBrowserLanguage();
+    if (!langFound && browserLanguage) {
+      langFound = _.find(this.LanguageList, function (localeItem) {
+        return localeItem.key === browserLanguage;
+      });
+    }
+
+    // last one is default EN
+    const defaultLanguage = _.first(this.LanguageList);
+
+    return langFound || defaultLanguage;
+  }
+
+  private detectBrowserLanguage() {
+    if (typeof window === 'undefined' || typeof window.navigator === 'undefined') {
+      return false;
+    }
+
+    let browserLang = window.navigator['languages'] ? window.navigator['languages'][0] : null;
+
+    browserLang = browserLang ||
+      window.navigator['languages'] ||
+      window.navigator['browserLanguage'] ||
+      window.navigator['userLanguage'];
+
+    if (browserLang.indexOf('-') !== -1) {
+      browserLang = browserLang.split('-')[0];
+    }
+
+    if (browserLang.indexOf('_') !== -1) {
+      browserLang = browserLang.split('_')[0];
+    }
+
+    return browserLang;
   }
 }
