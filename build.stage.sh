ae3ae2f208d76020d7b4e2945623d6c81bda57ca
geo <georgii.rychko@valor-software.com>
2017-02-14 09:55:00 +0200

    chore(build): add stage build script, fix dev build script

---
+++ build.stage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd /home
+
+eval "$(ssh-agent -s)"
+ssh-add vizabi_tools_deploy_ssh
+
+git clone git@github.com:Gapminder/ng2-tools-page.git -b master
+
+cd ng2-tools-page
+git pull origin master
+
+rm -rf node_modules
+
+npm i
+npm rebuild
+npm run build:stage
