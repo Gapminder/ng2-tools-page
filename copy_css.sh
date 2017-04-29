883638c4a475bc1b4977a379c37c832a972765f7
angieskazka <konzeptmeister@gmail.com>
2017-04-28 18:42:49 -0700

    chore(build): Try to fix deps import without dist and fail

--- copy_css.sh
+++ copy_css.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cp ./node_modules/vizabi/build/dist/vizabi.css ./src/assets/vizabi.css
-cp ./node_modules/vizabi-barrankchart/build/dist/barrankchart.css ./src/assets/barrankchart.css
-cp ./node_modules/vizabi-bubblechart/build/dist/bubblechart.css ./src/assets/bubblechart.css
-cp ./node_modules/vizabi-bubblemap/build/dist/bubblemap.css ./src/assets/bubblemap.css
-cp ./node_modules/vizabi-mountainchart/build/dist/mountainchart.css ./src/assets/mountainchart.css
-cp ./node_modules/vizabi-linechart/build/dist/linechart.css ./src/assets/linechart.css
+cp ./node_modules/vizabi/build/vizabi.css ./src/assets/vizabi.css
+cp ./node_modules/vizabi-barrankchart/build/barrankchart.css ./src/assets/barrankchart.css
+cp ./node_modules/vizabi-bubblechart/build/bubblechart.css ./src/assets/bubblechart.css
+cp ./node_modules/vizabi-bubblemap/build/bubblemap.css ./src/assets/bubblemap.css
+cp ./node_modules/vizabi-mountainchart/build/mountainchart.css ./src/assets/mountainchart.css
+cp ./node_modules/vizabi-linechart/build/linechart.css ./src/assets/linechart.css
