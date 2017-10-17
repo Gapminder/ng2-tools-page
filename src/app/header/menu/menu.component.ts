6bec9f8c4587ff62d1933775ea2d73cda788343e
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-10-17 18:03:49 +0300

    chore(ui): ui improvements
    Next issues were resolved:
    
    - add how to iframe when it expected -> before how to modal opening
    - Share buttons with prompts don't work on mobile
    
    Closes #85 #76

--- src/app/header/menu/menu.component.ts
+++ src/app/header/menu/menu.component.ts
@@ -1,7 +1,10 @@
 import {
-  ChangeDetectionStrategy, Component, EventEmitter, HostListener, Input, Output,
+  ChangeDetectionStrategy, Component, EventEmitter, HostListener, Input, Output, ViewChild,
   ViewEncapsulation
 } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { State } from '../../core/store';
+import { PromptForEmbeddedUrl, PromptForSharingLink } from '../../core/store/user-interaction/user-interaction.actions';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -11,12 +14,17 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
+  @ViewChild('howToContent') howToContent;
+  @ViewChild('howToMobileContent') howToMobileContent;
   @Input() menuItems: any[];
   @Input() selectedMenuItem: any;
 
   @Output() selectMenuItem: EventEmitter<any> = new EventEmitter();
 
   isHowToVisible = false;
+
+  constructor(private store: Store<State>) {
+  }
 
   getIconUrl(item): string {
     return `assets${item.icon_url}`;
@@ -28,6 +36,38 @@ export class MenuComponent {
 
   switchHowTo() {
     this.isHowToVisible = !this.isHowToVisible;
+
+    const howToContentEmpty = this.howToContent.nativeElement.children.length <= 0;
+    const howToMobileContentEmpty = this.howToMobileContent.nativeElement.children.length <= 0;
+
+    if (this.isHowToVisible) {
+      const content = document.createElement('div');
+      const contentMobile = document.createElement('div');
+      const vimeoContent = `<iframe src="https://player.vimeo.com/video/231885967"
+                                   class="how-to-frame"
+                                   webkitallowfullscreen
+                                   mozallowfullscreen
+                                   allowfullscreen></iframe>`;
+
+      content.innerHTML = vimeoContent;
+      contentMobile.innerHTML = vimeoContent;
+
+      if (howToContentEmpty) {
+        this.howToContent.nativeElement.appendChild(content);
+      }
+
+      if (howToMobileContentEmpty) {
+        this.howToMobileContent.nativeElement.appendChild(contentMobile);
+      }
+    }
+  }
+
+  getEmbeddedUrl(): void {
+    this.store.dispatch(new PromptForEmbeddedUrl());
+  }
+
+  shareLink(): void {
+    this.store.dispatch(new PromptForSharingLink());
   }
 
   @HostListener('window:resize', ['$event'])
