0fbe809b530db2a59ae89f2ba75459b881b50d09
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-10-19 15:57:43 +0300

    fixed(general): Bitly api is called on every state change
    Closes #101

--- src/app/core/bitly.service.ts
+++ src/app/core/bitly.service.ts
@@ -9,6 +9,7 @@ const RESPONSE_OK = 200;
 @Injectable()
 export class BitlyService {
   private bitlyUrl = 'https://api-ssl.bitly.com/v3/shorten';
+  private cache: {} = {};
 
   constructor(private http: Http) {
   }
@@ -20,10 +21,21 @@ export class BitlyService {
 
     const serviceUrl = `${this.bitlyUrl}?access_token=${'c5c5bdef4905a307a3a64664b1d06add09c48eb8'}&longUrl=${encodeURIComponent(url)}`;
 
+    if (this.cache[serviceUrl]) {
+      return Observable.of(this.cache[serviceUrl]);
+    }
+
     return this.http.get(serviceUrl).map(response => {
       const bitlyResponse = response.json();
 
-      return bitlyResponse.status_code === RESPONSE_OK ? bitlyResponse.data.url : window.location;
+      let result: any = window.location;
+
+      if (bitlyResponse.status_code === RESPONSE_OK) {
+        this.cache[serviceUrl] = bitlyResponse.data.url;
+        result = this.cache[serviceUrl];
+      }
+
+      return result;
     });
   }
 }
