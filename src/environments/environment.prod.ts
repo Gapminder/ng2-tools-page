5b1f1fa7c75645bbcef138500dbed61ebd3faf15
angieskazka <konzeptmeister@gmail.com>
2017-10-28 16:03:52 +0200

    Revert "chore(env): Switch Tools page to WS dev temporarily"
    This reverts commit f79950083b7a6d4acd6d76f7cd1173729b116ffa.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsUrl: 'https://waffle-server-dev.gapminderdev.org',
-  wsUrlAssets: 'https://import-waffle-server-dev.gapminderdev.org',
+  wsUrl: 'https://waffle-server.gapminder.org',
+  wsUrlAssets: 'https://import-waffle-server.gapminder.org',
   datasetBranch: ''
 };
