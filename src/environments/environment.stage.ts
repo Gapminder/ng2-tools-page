ec4af3958ce717eebf86a686fc77a8e79986faae
angieskazka <konzeptmeister@gmail.com>
2017-12-14 11:53:14 +0200

    chore(envs): Change IPs to DNS names of waffle-server-stage

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  wsUrl: 'http://23.251.135.254',
-  wsUrlAssets: 'http://35.195.119.199',
+  wsUrl: 'https://waffle-server-stage.gapminder.org',
+  wsUrlAssets: 'https://import-waffle-server-stage.gapminder.org',
   datasetBranch: ''
 };
